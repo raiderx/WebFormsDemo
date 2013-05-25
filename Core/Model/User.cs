d9b524235453e646887e9b0c674a8e3b0958fa54
Pavel Karpukhin <karpukhin.pavel@gmail.com>
2013-05-25 21:08:31 +0400

    Model class 'User' was added.

--- Core/Model/User.cs
+++ Core/Model/User.cs
@@ -1,12 +1,12 @@
 using System;
 
-namespace Core
+namespace Core.Model
 {
 	public class User
 	{
-		public User ()
-		{
-		}
+		public virtual string FirstName { get; set; }
+		public virtual string LastName { get; set; }
+		public virtual string Email { get; set; }
 	}
 }
 
